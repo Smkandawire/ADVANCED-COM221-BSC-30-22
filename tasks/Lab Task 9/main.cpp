ffe0271cc2bed5686897b6d73d0e55541d1ce1f6
Smkandawire <167068447+Smkandawire@users.noreply.github.com>
2024-06-08 14:19:22 +0200

    added codes in task 9

--- tasks/Lab Task 9/main.cpp
+++ tasks/Lab Task 9/main.cpp
@@ -1,17 +1,12 @@
-#include <iostream> 
-#include "Rectangle.h"
-#include "Triangle.h"
-
-
-using namespace std; 
-  int main() { 
-        Rectangle rect; 
-        Triangle trgl; 
-
-        rect.SetValues(4,5); 
-        trgl.SetValues (4,5);
-
-        cout << rect.Area() << '\n'; 
-        cout << trgl.Area() << '\n'; 
-        return 0; 
+#include <iostream>
+#include "Daughter.h"
+#include "Son.h"
+using namespace std;
+int main () 
+{
+ 
+ Daughter theDaughter(1);
+ Son theSon(2);
+ 
+ return 0;
 }
